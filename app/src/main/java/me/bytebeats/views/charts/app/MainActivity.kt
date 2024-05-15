1169a7e6fcc62a98d16c4d4f2db0168f5f96ea0e
Roberto Leinardi <leinardi@gmail.com>
2024-05-15 09:09:32 +0200

    Bump dependencies to latest stable

--- app/src/main/java/me/bytebeats/views/charts/app/MainActivity.kt
+++ app/src/main/java/me/bytebeats/views/charts/app/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import me.bytebeats.views.charts.app.ui.ComposeCharts
 
@@ -22,12 +19,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    val test by remember {
-        mutableStateOf("")
-    }
-
-    val collect by remember {
-        mutableStateOf(mutableListOf<String>())
-    }
     ComposeCharts()
 }
